770bcb88b4410a5dd5e2f99ba394c72c2924ed81
Marco Andronaco <andronacomarco@gmail.com>
2026-02-12 21:41:38 +0100

    Change instagram api base

--- src/platforms/instagram/instagram-schemas.ts
+++ src/platforms/instagram/instagram-schemas.ts
@@ -3,4 +3,4 @@ export const INSTAGRAM_MAX_IMAGES = 10; // carousel limit
 export const INSTAGRAM_MAX_IMAGE_SIZE = 8 * 1024 * 1024; // 8 MB
 export const INSTAGRAM_MAX_VIDEO_SIZE = 100 * 1024 * 1024; // 100 MB
 
-export const INSTAGRAM_GRAPH_API_BASE = 'https://graph.facebook.com/v21.0';
+export const INSTAGRAM_GRAPH_API_BASE = 'https://graph.instagram.com/v21.0';
